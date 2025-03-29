3624ba3881ca09ef041335002a874bb5a286fb6a
silver <53583492+SilverArgente@users.noreply.github.com>
2025-03-28 21:44:05 -0400

    fixed canvas scaling bug for view

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 ctx.fillStyle = "green";
 ctx.fillRect(10, 10, 150, 100);
