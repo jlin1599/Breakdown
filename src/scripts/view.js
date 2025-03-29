2f520eafe61111878a42531bf61933d556876c5d
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-03-29 14:22:18 -0400

    implemented json parsing into tree

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,3 +1,51 @@
+// import { Concept } from './Concept.js'
+// import { Tree } from './Tree.js' 
+
+// Putting all classes in 1 file (for now until frontend server is up and running)
+
+// Concept is like a "node" or vertex within a tree
+class Concept {
+
+    title;
+    description;
+    children; // Array of Concepts
+
+    constructor(title, description, children) {
+        this.title = title;
+        this.description = description;
+        this.children = children;
+    }
+
+    addChild(child) {
+        this.children.append(child)
+    }
+
+    toString() {
+        return `${this.title}: ${this.description}, children = ${children}`;
+    }
+    
+}
+
+class Tree {
+    
+    root; // Array of Concepts, representing topmost concepts in tree
+
+    constructor(root) {
+        this.root = root;
+    }
+
+    toString() {
+        return `${this.title}: ${this.description}, children = ${children}`;
+    }
+
+
+}
+
+
+
+// Start of actual view.js code
+
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -86,5 +134,86 @@ function drawWireframe() {
         ctx.stroke();
     }
 }
+
+// Create a tree from parsing json file
+parsed_pdf = {
+    "Vehicle": {
+        description: "Anything that can transport people",
+        children: ["Car", "Train", "Plane"]
+    },
+    "Car": {
+        description: "A road vehicle with four wheels, operating by a driver",
+        children: []
+    },
+    "Train": {
+        description: "A long distance method for transporting people and cargo",
+        children: []
+    },
+    "Plane": {
+        description: "A flying vehicle, for extreme long distance overseas travel",
+        children: []
+    }
+}
+
+// Recursively construct tree from json file
+
+// Get individual concept and information from json file, given the title and json file
+function getConcept(title, file) {
+    return file[title] || null;
+}
+
+function createNode(title, file) {
+    const child_info = getConcept(title, file);
+
+    return new Concept(title, child_info.description, []);
+}
+
+function createTreeHelper(concept, parsed_pdf) {
+
+    const node = createNode(concept, parsed_pdf);
+
+
+    if (node.children.length === 0)
+        return node;
+
+    for (child in node.children) {
+        node.children.push( createTreeHelper(child, parsed_pdf) );
+    }
+
+    return node;
+}
+
+function createTree(parsed_pdf) {
+
+    let root, children;
+
+    // Get first object to be root
+    for (var [title, data] of Object.entries(parsed_pdf)) {
+        root = new Concept(title, data.description, []);
+        children = data.children;
+        break;
+    }
+
+    // Recursively define children nodes
+    if (children.length === 0)
+        return root;
+
+    for (child in children) { // Child is not the element in the children array, but the index (because javascript sucks!)
+        
+        root.children.push( createTreeHelper(children[child], parsed_pdf) );
+    }
+
+    return root;
+
+    
+
+}
+
+// createNode("Vehicle", parsed_pdf);
+root = createTree(parsed_pdf);
+const concept_hierarchy = new Tree(root);
+;
+console.log(concept_hierarchy)
+
 
 draw();
